339532a4db4e64f18ae9fd5fcb22f98f04a1477f
yohannesluminari <y.luminari@gmail.com>
2024-05-06 14:38:59 +0200

    branch1

--- src/main/java/it.epicodeGruppo2/Tratta.java
+++ src/main/java/it.epicodeGruppo2/Tratta.java
@@ -1,5 +1,57 @@
 package it.epicodeGruppo2;
 
 public class Tratta {
+    private String zonaPartenza;
+    private String capolinea;
+    private int numeroPercorsoPercorsi;
+    private double tempoTragitto;
 
+    @Override
+    public String toString() {
+        return "Tratta{" +
+                "zonaPartenza='" + zonaPartenza + '\'' +
+                ", capolinea='" + capolinea + '\'' +
+                ", numeroPercorsoPercorsi=" + numeroPercorsoPercorsi +
+                ", tempoTragitto=" + tempoTragitto +
+                '}';
+    }
+
+    public Tratta(String zonaPartenza, String capolinea, int numeroPercorsoPercorsi, double tempoTragitto) {
+        this.zonaPartenza = zonaPartenza;
+        this.capolinea = capolinea;
+        this.numeroPercorsoPercorsi = 0;
+        this.tempoTragitto = 0;
+    }
+
+    public String getZonaPartenza() {
+        return zonaPartenza;
+    }
+
+    public void setZonaPartenza(String zonaPartenza) {
+        this.zonaPartenza = zonaPartenza;
+    }
+
+    public String getCapolinea() {
+        return capolinea;
+    }
+
+    public void setCapolinea(String capolinea) {
+        this.capolinea = capolinea;
+    }
+
+    public int getNumeroPercorsoPercorsi() {
+        return numeroPercorsoPercorsi;
+    }
+
+    public void setNumeroPercorsoPercorsi(int numeroPercorsoPercorsi) {
+        this.numeroPercorsoPercorsi = numeroPercorsoPercorsi;
+    }
+
+    public double getTempoTragitto() {
+        return tempoTragitto;
+    }
+
+    public void setTempoTragitto(double tempoTragitto) {
+        this.tempoTragitto = tempoTragitto;
+    }
 }
