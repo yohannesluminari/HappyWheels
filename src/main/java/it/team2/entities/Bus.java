267034c447f480d7275ad92b75e192cb6bb3638e
yohannesluminari <y.luminari@gmail.com>
2024-05-07 16:54:17 +0200

    Veichle Maintence

---
+++ src/main/java/it/team2/entities/Bus.java
@@ -0,0 +1,9 @@
+package it.team2.entities;
+
+import java.util.Date;
+
+public class Bus extends Vehicle {
+    private int capacity;
+
+
+}
---
+++ src/main/java/it/team2/entities/Tram.java
@@ -0,0 +1,10 @@
+package it.team2.entities;
+
+import java.util.Date;
+
+public class Tram extends Vehicle {
+    private int capacity;
+
+
+
+}
