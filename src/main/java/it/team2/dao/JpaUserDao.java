70ba4816dc5d1a5936b5a1ae7a64f0c6515188fb
Cristur <cristianoturri@gmail.com>
2024-05-08 10:44:28 +0200

    Committino

--- src/main/java/it/team2/dao/JpaUserDao.java
+++ src/main/java/it/team2/dao/JpaUserDao.java
@@ -4,22 +4,23 @@ import it.team2.entities.User;
 
 import java.util.Optional;
 
-public class JpaUserDao extends JpaBaseDao implements UserDao{
+public class JpaUserDao extends JpaBaseDao implements UserDao {
     @Override
     public void saveUser(User user) {
 
-        try{
-           var t = em.getTransaction();
-           t.begin();
-           em.persist(user);
-           t.commit();
-        }catch (Exception ex){
-            log.error("Ciuaone mona" , ex);
+        try {
+            var t = em.getTransaction();
+            t.begin();
+            em.persist(user);
+            t.commit();
+        } catch (Exception ex) {
+            log.error("Ciuaone mona", ex);
         }
     }
 
     @Override
     public Optional<User> getUserById(long id) {
+
         return Optional.ofNullable(em.find(User.class, id));
     }
 
